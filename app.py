a4a1c48ad9e7a642ed72d15b3f9c6b79890259f2
pk7745 <pk9396247@gmail.com>
2026-02-09 18:37:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -282,6 +282,10 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == '__main__':
-    app.run()
+    # '0.0.0.0' tells the server to accept external connections
+    # os.environ.get('PORT') is required for deployment platforms
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
