3ed96e75c98964ccd99e1987d679414a6972f432
pk7745 <pk9396247@gmail.com>
2026-02-15 14:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -386,8 +386,8 @@ def add_employee():
             join_date=request.form.get('join_date', '2023-01-01'),
             caste=request.form.get('caste'),
             religion=request.form.get('religion'),
-            dept: request.form.get('dept'),       # New Field
-            dept_id: request.form.get('dept_id'),
+            dept=request.form.get('dept'),       # New Field
+            dept_id=request.form.get('dept_id'),
         )
         db.session.add(new_user)
         db.session.commit()
@@ -1049,6 +1049,7 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
 
